4cf7f265d22c5b928d399a7adad43fc96ad70cf0
Omer Sabah <omerrj99@gmail.com>
2021-05-31 14:53:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // Add your functions here
 
 
+
+for (let i = 0; i < src.length; i++) {
+    let theElement = src[i]
+    r.push(cb(theElement))
+  }
+
+  return r;
+}
+
 function reduce(src, cb, starting){
   let r = (!!starting) ? starting : src[0]
   let i = (!!starting) ? 0 : 1
